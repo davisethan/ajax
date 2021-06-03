ea032cc45bbddf46e2bf1fcf209d37cf317e6d34
davisethan <davisethan.work@gmail.com>
2021-06-03 11:45:47 -0700

    UI/DB finished

---
+++ public/app.js
@@ -0,0 +1,228 @@
+function htmlPostCreate(){
+    // Get create form
+    var form = document.getElementById('new-workout');
+    
+    // Add create form event listener
+    form.addEventListener('submit',function(e){
+        e.preventDefault();
+        var req = new XMLHttpRequest();
+        var name = document.getElementById('new-name').value;
+        var reps = document.getElementById('new-reps').value;
+        var weight = document.getElementById('new-weight').value;
+        var date = document.getElementById('new-date').value;
+        var lbs = document.getElementById('new-unit').value === 'lbs' ? 1:0;
+        var url = `http://localhost:3000/?query=create&name=${name}&reps=${reps}&weight=${weight}&date=${date}&lbs=${lbs}`;
+        req.onload = function(){
+            if(req.status == 200){
+                // Update HTML table with new workout after ok http request
+                var workout = {
+                    id: req.responseText,
+                    name: name,
+                    reps: reps,
+                    weight: weight,
+                    date: date,
+                    lbs: lbs
+                };
+                appendTableRow(workout);
+                // Clear create form
+                document.getElementById('new-name').value = null;
+                document.getElementById('new-reps').value = null;
+                document.getElementById('new-weight').value = null;
+                document.getElementById('new-date').value = null;
+                document.getElementById('new-unit').value = 'lbs';
+            }
+        }; 
+        req.open('POST',url,true);
+        req.send();
+    });
+}
+
+function htmlPostRead(){
+    var req = new XMLHttpRequest();
+    var url = 'http://localhost:3000/?query=read';
+    req.onload = function(){
+        if(req.status === 200){
+            var workouts = JSON.parse(req.responseText);
+            workouts.forEach(function(workout){
+                appendTableRow(workout);
+            });
+        }
+    }
+    req.open('POST',url,true);
+    req.send();
+}
+
+function appendTableRow(workout){
+    var id = workout.id;
+    var name = workout.name;
+    var reps = workout.reps;
+    var weight = workout.weight;
+    var date = workout.date;
+    var lbs = workout.lbs;
+
+    // Get table
+    var table = document.getElementById('workouts-table');
+
+    // Append table row
+    var tableRow = document.createElement('div');
+    tableRow.style.display = 'table-row';
+    tableRow.width = '100%';
+    tableRow.id = `row-${id}`;
+    table.appendChild(tableRow);
+
+    // Append id table cell
+    var idCell = document.createElement('div');
+    idCell.style.display = 'table-cell';
+    var idInput = document.createElement('input');
+    idInput.id = `id-${id}`;
+    idInput.type = 'hidden';
+    idInput.name = 'id';
+    idInput.value = `${id}`;
+    idCell.appendChild(idInput);
+    tableRow.appendChild(idCell);
+
+    // Append name table cell
+    var nameCell = document.createElement('div');
+    nameCell.style.display = 'table-cell';
+    var nameInput = document.createElement('input');
+    nameInput.id = `name-${id}`;
+    nameInput.type = 'text';
+    nameInput.name = 'name';
+    nameInput.value = `${name}`;
+    nameCell.appendChild(nameInput);
+    tableRow.appendChild(nameCell);
+
+    // Append reps table cell
+    var repsCell = document.createElement('div');
+    repsCell.style.display = 'table-cell';
+    var repsInput = document.createElement('input');
+    repsInput.id = `reps-${id}`;
+    repsInput.type = 'number';
+    repsInput.name = 'reps';
+    repsInput.value = `${reps}`;
+    repsCell.appendChild(repsInput);
+    tableRow.appendChild(repsCell);
+
+    // Append weight table cell
+    var weightCell = document.createElement('div');
+    weightCell.style.display = 'table-cell';
+    var weightInput = document.createElement('input');
+    weightInput.id = `weight-${id}`;
+    weightInput.type = 'number';
+    weightInput.name = 'weight';
+    weightInput.value = `${weight}`;
+    weightCell.appendChild(weightInput);
+    tableRow.appendChild(weightCell);
+
+    // Append date table cell
+    var dateCell = document.createElement('div');
+    dateCell.style.display = 'table-cell';
+    var dateInput = document.createElement('input');
+    dateInput.id = `date-${id}`;
+    dateInput.type = 'date';
+    dateInput.name = 'date';
+    dateInput.value = date;
+    dateCell.appendChild(dateInput);
+    tableRow.appendChild(dateCell);
+
+    // Append unit table cell
+    var unitCell = document.createElement('div');
+    unitCell.style.display = 'table-cell';
+    // Form select
+    var unitSelect = document.createElement('select');
+    unitSelect.id = `unit-${id}`;
+    unitSelect.name = 'unit';
+    // Form select lbs option
+    var unitOptionLbs = document.createElement('option');
+    unitOptionLbs.value = 'lbs';
+    unitOptionLbs.textContent = 'lbs';
+    // Form select kgs option
+    var unitOptionKgs = document.createElement('option');
+    unitOptionKgs.value = 'kgs';
+    unitOptionKgs.textContent = 'kgs';
+    // Append select options
+    if(lbs === 1){
+        unitSelect.appendChild(unitOptionLbs);
+        unitSelect.appendChild(unitOptionKgs);
+    }else{
+        unitSelect.appendChild(unitOptionKgs);
+        unitSelect.appendChild(unitOptionLbs);
+    }
+    unitCell.appendChild(unitSelect);
+    tableRow.appendChild(unitCell);
+
+    // Append update table cell
+    var updateCell = document.createElement('div');
+    updateCell.style.display = 'table-cell';
+    var updateInput = document.createElement('input');
+    updateInput.id = `update-${id}`;
+    updateInput.type = 'button';
+    updateInput.name = 'update';
+    updateInput.value = 'Update';
+    updateCell.appendChild(updateInput);
+    tableRow.appendChild(updateCell);
+
+    // Add update event listener
+    updateInput.addEventListener('click',function(e){
+        e.preventDefault();
+        var workout = {
+            id: idInput.value,
+            name: nameInput.value,
+            reps: repsInput.value,
+            weight: weightInput.value,
+            date: dateInput.value,
+            lbs: unitSelect.value === 'lbs' ? 1:0
+        };
+        htmlPostUpdate(workout);
+    });
+
+    // Append delete table cell
+    var deleteCell = document.createElement('div');
+    deleteCell.style.display = 'table-cell';
+    var deleteInput = document.createElement('input');
+    deleteInput.id = `delete-${id}`;
+    deleteInput.type = 'button';
+    deleteInput.name = 'delete';
+    deleteInput.value = 'Delete';
+    deleteCell.appendChild(deleteInput);
+    tableRow.appendChild(deleteCell);
+
+    // Add delete event listener
+    deleteInput.addEventListener('click',function(e){
+        e.preventDefault();
+        // Client delete workout
+        var index = Array.from(table.children).reduce(function(acc,val,idx){
+            return val.id === `row-${idInput.value}` ? acc+idx:acc;
+        },0);
+        table.removeChild(table.children[index]);
+        // Server delete workout
+        htmlPostDelete(idInput.value);
+    });
+}
+
+function htmlPostUpdate(workout){
+    var req = new XMLHttpRequest();
+    var id = workout.id;
+    var name = workout.name;
+    var reps = workout.reps;
+    var weight = workout.weight;
+    var date = workout.date;
+    var lbs = workout.lbs;
+    var url = `http://localhost:3000/?query=update&id=${id}&name=${name}&reps=${reps}&weight=${weight}&date=${date}&lbs=${lbs}`;
+    req.open('POST',url,true);
+    req.send();
+}
+
+function htmlPostDelete(id){
+    var req = new XMLHttpRequest();
+    var url = `http://localhost:3000/?query=delete&id=${id}`;
+    req.open('POST',url,true);
+    req.send();
+}
+
+/**
+ * Main
+ */
+
+htmlPostCreate();
+htmlPostRead();
---
+++ server.js
@@ -0,0 +1,111 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var mysql = require('./dbcon.js');
+var path = require('path');
+
+var app = express();
+app.set('port',process.argv[2]);
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/',function(req,res,next){
+    res.sendFile(path.join(__dirname,'/public/index.html'));
+});
+
+app.post('/',function(req,res,next){
+    // Read all workouts
+    if(req.query.query === 'read'){
+        mysql.pool.query('SELECT * FROM workouts',function(err,rows){
+            if(err){
+                next(err);
+                return;
+            }
+            rows = rows.map(function(row){
+                var date = row.date.toISOString();
+                date = date.slice(0,date.indexOf('T'));
+                row.date = date;
+                return row;
+            });
+            res.json(rows);
+        });
+    // Create new workout
+    }else if(req.query.query === 'create'){
+        var query = 'INSERT INTO workouts (name,reps,weight,date,lbs) VALUES (?,?,?,?,?)';
+        var params = [
+        req.query.name,
+        req.query.reps,
+        req.query.weight,
+        req.query.date,
+        req.query.lbs];
+        mysql.pool.query(query,params,function(err,result){
+            if(err){
+                next(err);
+                return;
+            }
+            res.json(result.insertId);
+        });
+    // Update workout
+    }else if(req.query.query === 'update'){
+        var query = 'SELECT * FROM workouts WHERE id=?';
+        var params = [req.query.id];
+        mysql.pool.query(query,params,function(err,result){
+            var curvals = result[0];
+            var query = 'UPDATE workouts SET name=?,reps=?,weight=?,date=?,lbs=? WHERE id=?';
+            var params = [
+            req.query.name || curvals.name,
+            req.query.reps || curvals.reps,
+            req.query.weight || curvals.weight,
+            req.query.date || curvals.date,
+            req.query.lbs || curvals.lbs,
+            req.query.id || curvals.id];
+            mysql.pool.query(query,params,function(err,result){
+                if(err){
+                    next(err);
+                    return;
+                }
+            });
+        });
+    // Delete workout
+    }else if(req.query.query === 'delete'){
+        var query = 'DELETE FROM workouts WHERE id=?';
+        var params = [req.query.id];
+        mysql.pool.query(query,params,function(err,result){
+            if(err){
+                next(err);
+                return;
+            }
+        });
+    }
+});
+
+/**
+ * Clear workouts table
+ */
+app.get('/reset-table',function(req,res,next){
+    mysql.pool.query('DROP TABLE IF EXISTS workouts',function(err){
+        if(err){
+            next(err);
+            return;
+        }
+        var query = `CREATE TABLE workouts(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        reps INT,
+        weight INT,
+        date DATE,
+        lbs BOOLEAN)`;
+        mysql.pool.query(query,function(err){
+            if(err){
+                next(err);
+                return;
+            }
+            res.sendFile(path.join(__dirname,'/public/index.html'));
+        });
+    });
+});
+
+app.listen(app.get('port'),function(){
+    console.log(`Visit http://localhost:${app.get('port')}`);
+});
